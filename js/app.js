fbdb5657335943ea90dec5517269c6a40c5a50ff
apolios <airmoss@gmail.com>
2019-02-14 02:26:53 +0100

    added animations and parallax effect when scrolling to header

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+// welcome section handling
+const welcomeSection = document.querySelector(".welcome > .header");
+
+const scrollsSite = () =>{
+  welcomeSection.style.transform = `translateY(${window.scrollY/1.5}px)`
+  console.log(window.scrollY);
+}
+
+window.onload = () =>{
+  welcomeSection.classList.add("header--changed");
+  setTimeout(() => {
+    welcomeSection.classList.remove("header--changed");
+    welcomeSection.style.opacity = 1;
+    welcomeSection.style.transform = "none";
+    welcomeSection.style.transition = "none";
+    window.addEventListener("scroll", scrollsSite);
+   }, 3500)
+}
+
 // about section handling
 
 const aboutSectionParagraph = document.querySelector("#about.info");
