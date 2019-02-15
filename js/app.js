60677b07e8c1bed484ad85a89937f62e3d21d94e
apolios <airmoss@gmail.com>
2019-02-15 03:59:46 +0100

    added contact form and some functionality

--- js/app.js
+++ js/app.js
@@ -2,16 +2,15 @@
 const welcomeSection = document.querySelector(".welcome > .header");
 
 const scrollsSite = () =>{
+  if(window.scrollY < window.innerHeight){
   welcomeSection.style.transform = `translateY(${window.scrollY/1.5}px)`
-  console.log(window.scrollY);
+  console.log(window.scrollY, window.innerHeight);
+  }
 }
 
 window.onload = () =>{
   welcomeSection.classList.add("header--changed");
   setTimeout(() => {
-    welcomeSection.classList.remove("header--changed");
-    welcomeSection.style.opacity = 1;
-    welcomeSection.style.transform = "none";
     welcomeSection.style.transition = "none";
     window.addEventListener("scroll", scrollsSite);
    }, 3500)
@@ -26,3 +25,16 @@ aboutSectionBtn.addEventListener("click", () =>{
   aboutSectionParagraph.classList.toggle("about--changed");
   aboutSectionParagraph.classList.contains("about--changed") ? aboutSectionBtn.textContent = "Show less" : aboutSectionBtn.textContent = "Read more"
 })
+
+// contact section handling
+
+const textarea = document.querySelector('.contact__textarea');
+
+const autoResize = () =>{
+  setTimeout(() =>{
+    textarea.style.cssText = 'height:auto; padding:0';
+    textarea.style.cssText = 'height:' + textarea.scrollHeight + 'px';
+  },0);
+}
+
+textarea.addEventListener('keydown', () => autoResize());
